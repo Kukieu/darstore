cb9400db07a971ac2d9a9dbc46927d60b33b8318
Michał Balicki <stiekerosiem@gmail.com>
2017-09-02 12:15:44 +0200

    footer

--- resources/templates/front/footer.php
+++ resources/templates/front/footer.php
@@ -4,16 +4,23 @@
 
         <!-- Footer -->
         <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <a href="https://www.youtube.com/channel/UCjn9l43xn4vgen_UK50TP3g" class="rounded float-right" target="_blank">
-                        <img src="images/yt.png" alt="youtube channel" style="width:42px;height:42px;border:0;">
-                    </a>
-                    <a href="https://www.facebook.com/" class="rounded float-right" target="_blank">
-                        <img src="images/fb.png" alt="facebook page" style="width:42px;height:42px;border:0;">
-                    </a>
-                </div>
-            </div>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="css/footer.css" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet" integrity="sha256-MfvZlkHCEqatNoGiOXveE8FIwMzZg4W85qfrfIFBfYc= sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+<!-- // -->
+<div class="container">
+    <hr>
+        <div class="text-center center-block">
+            <br />
+                <a href="https://www.facebook.com/bootsnipp"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a>
+	            <a href="https://twitter.com/bootsnipp"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a>
+	            <a href="https://www.youtube.com/channel/UCjn9l43xn4vgen_UK50TP3g"><i id="social-gp" class="fa fa-youtube-square fa-3x social"></i></a>
+	            <a href="mailto:bootsnipp@gmail.com"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a>
+</div>
+    <hr>
+</div>
+
+<br />
         </footer>
 
     </div>
